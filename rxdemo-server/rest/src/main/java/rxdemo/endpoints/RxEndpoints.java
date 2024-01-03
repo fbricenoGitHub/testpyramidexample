c17216b80b458c7083b2e5a58cecb2f3a2dd2f31
Fernando <fbriceno@emida.net>
2024-01-02 22:29:43 -0500

    Bambbo trigger build from master

--- rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
+++ rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
@@ -11,6 +11,9 @@ import spark.Response;
 import static spark.Spark.*;
 
 // change to be deployed
+
+// another change to see update from master to course branch
+
 public class RxEndpoints {
    public static void initalizeEndpoints() {
 
